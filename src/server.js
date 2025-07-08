e3a077417c5471395597377f95c9d4b24091c04b
hanna-uler <hannauler@gmail.com>
2025-07-08 12:01:23 -0400

    refactor server.js, add studentsRoute

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllStudents, getStudentById } from './services/students.js';
+import studentsRouter from './routers/students.js';
 
 const PORT = Number(getEnvVar("PORT", "3000"));
 
@@ -26,25 +26,7 @@ export const startServer = () => {
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
-    res.status(200).json({
-      data: students.at,
-    });
-  });
-  app.get('/students/:studentId', async (req, res) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-    if (!student) {
-      res.status(404).json({
-        message: "Student is not found"
-      });
-      return;
-    }
-    res.status(200).json({
-      data: student,
-    });
-  });
+  app.use(studentsRouter);
 
   app.use((req, res, next) => {
     res.status(404).json({
