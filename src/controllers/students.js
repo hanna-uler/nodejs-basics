5b45b5bdbb8d44855f19cbaf880dd8e29dc717e6
hanna-uler <hannauler@gmail.com>
2025-07-08 12:10:01 -0400

    move controller funcs to src/controllers/students.js

---
+++ src/controllers/students.js
@@ -0,0 +1,28 @@
+import { getAllStudents, getStudentById } from "../services/students.js";
+
+export const getStudentsController = async (req, res) => {
+    const students = await getAllStudents();
+    res.json({
+        status: 200,
+        message: 'Successfully found students!',
+        data: students,
+    });
+};
+
+export const getStudentByIdController = async (req, res) => {
+    const { studentId } = req.params;
+    const student = await getStudentById(studentId);
+
+    if (!student) {
+        res.status(404).json({
+            message: 'Student not found'
+        });
+        return;
+    }
+
+    res.json({
+        status: 200,
+        message: `Successfully found student with id ${studentId}!`,
+        data: student,
+    });
+};
