465f7631776c7748f45e61712ded04aec154f6ae
hanna-uler <hannauler@gmail.com>
2025-07-08 12:24:17 -0400

    add controllers try-catch wrapper

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -9,17 +9,22 @@ export const getStudentsController = async (req, res) => {
     });
 };
 
-export const getStudentByIdController = async (req, res) => {
+export const getStudentByIdController = async (req, res, next) => {
     const { studentId } = req.params;
     const student = await getStudentById(studentId);
 
+    // prev code:
+    // if (!student) {
+    //     res.status(404).json({
+    //         message: 'Student not found'
+    //     });
+    //     return;
+    // }
+
     if (!student) {
-        res.status(404).json({
-            message: 'Student not found'
-        });
+        next(new Error("Student is not found"));
         return;
     }
-
     res.json({
         status: 200,
         message: `Successfully found student with id ${studentId}!`,
--- src/routers/students.js
+++ src/routers/students.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { getStudentsController, getStudentByIdController } from '../controllers/students.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
-router.get('/students', getStudentsController);
+router.get('/students', ctrlWrapper(getStudentsController));
 
-router.get('/students/:studentId', getStudentByIdController);
+router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 
 export default router;
