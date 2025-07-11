b28aff469b4601565f5430c4b4fbed303df7e859
hanna-uler <hannauler@gmail.com>
2025-07-11 11:59:10 -0400

    add validation for student creating and upd

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { getStudentsController, getStudentByIdController, createStudentController, deleteStudentController, upsertStudentController, patchStudentController } from '../controllers/students.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createStudentSchema, updateStudentSchema } from '../validation/students.js';
 
 const router = Router();
 
@@ -8,12 +10,12 @@ router.get('/students', ctrlWrapper(getStudentsController));
 
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 
-router.post('/students', ctrlWrapper(createStudentController));
+router.post('/students', validateBody(createStudentSchema), ctrlWrapper(createStudentController));
 
 router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
 
-router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
+router.put('/students/:studentId', validateBody(createStudentSchema), ctrlWrapper(upsertStudentController));
 
-router.patch('/students/:studentId', ctrlWrapper(patchStudentController));
+router.patch('/students/:studentId', validateBody(updateStudentSchema), ctrlWrapper(patchStudentController));
 
 export default router;
