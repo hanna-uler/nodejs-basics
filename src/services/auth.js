f580d43bd2bed527d644259296c4a2163923181d
hanna-uler <hannauler@gmail.com>
2025-07-16 21:14:27 -0400

    add create session

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
-import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
-import { UsersCollection } from "../db/models/user.js";
+import bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
+import { UsersCollection } from '../db/models/user.js';
+import { SessionCollection } from '../db/models/session.js';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
 
 export const registerUser = async (payload) => {
     const user = await UsersCollection.findOne({ email: payload.email });
@@ -19,4 +22,15 @@ export const loginUser = async (payload) => {
     if (!isEqual) {
         throw createHttpError(401, "Unauthorized");
     }
+    await SessionCollection.deleteOne({ userId: user._id });
+    const accessToken = randomBytes(30).toString("base64");
+    const refreshToken = randomBytes(30).toString("base64");
+
+    return await SessionCollection.create({
+        userId: user._id,
+        accessToken,
+        refreshToken,
+        accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+        refreshTokenValidUntil: new Date(Date.now() + ONE_DAY)
+    });
 };
